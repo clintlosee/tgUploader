d35a9dea1d8925b47ae1ee64c21b6146a631c3ec
Clint Losee <clint.losee@gmail.com>
2020-02-27 11:50:51 -0700

    updated mongoose

--- start.js
+++ start.js
@@ -14,10 +14,10 @@ require('dotenv').config({ path: 'variables.env' });
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') {
   console.log('Connecting to dev DB')
-  mongoose.connect(process.env.DATABASE_DEV); // Dev
+  mongoose.connect(process.env.DATABASE_DEV, { useNewUrlParser: true, useUnifiedTopology: true }); // Dev
 } else {
   console.log('Connecting to prod DB')
-  mongoose.connect(process.env.DATABASE); // Production
+  mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }); // Production
 }
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
